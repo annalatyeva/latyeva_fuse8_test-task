b529781b50fb81153a656c5aeb75e6244e14f378
annalatyeva <anysha_s@mail.ru>
2025-02-09 15:41:38 +0500

    feat: add fetch functionallity

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -3,10 +3,33 @@ import { useState } from 'react';
 const Input = () => {
   const placeholderText = 'Search characters...';
 
+  async function getCharacterInfo(inputValue) {
+    try {
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character/?name=${inputValue}`
+      );
+
+      if (response.ok) {
+        const json = await response.json();
+        console.log(json);
+      } else {
+        if (response.status === 404) throw new Error('404, Not found');
+        if (response.status === 500)
+          throw new Error('500, internal server error');
+        if (!response.ok) throw new Error(response.status);
+      }
+    } catch (error) {
+      console.error('Fetch', error);
+    }
+  }
+
   const [inputValue, setInputValue] = useState('');
   const getInputValue = (event) => {
     const { value } = event.target;
     setInputValue(value);
+    if (value.length > 3) {
+      getCharacterInfo(inputValue);
+    }
   };
 
   return (
